35cbfbcff69c3cab59b73d41a85958c77537bac6
Muhammad Mairaj <muhammadmairajj@gmail.com>
2023-10-23 15:39:13 +0500

    first commit

---
+++ src/app/(CSR)/search/page.tsx
@@ -0,0 +1,9 @@
+import SearchPage from "./SearchPage";
+
+export const metadata = {
+    title: "Search - NextJS 13.5 Image Gallery",
+}
+
+export default function Page() {
+    return <SearchPage />
+}
